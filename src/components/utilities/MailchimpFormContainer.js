326d6858db94f43e84082d19d1cce895790d03d2
Alexander <alex@detourdigital.ca>
2022-06-24 15:22:38 -0600

    set proper widths for primary content

--- src/components/utilities/MailchimpFormContainer.js
+++ src/components/utilities/MailchimpFormContainer.js
@@ -2,6 +2,6 @@ import MailchimpSubscribe from "react-mailchimp-subscribe"
 
 const url = `https://camrosefcss.us20.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
-const SimpleForm = () => <MailchimpSubscribe url={url}/>
+const SimpleForm = () => <div className="mailchimp-container"><MailchimpSubscribe url={url}/></div>
 
 export default SimpleForm;
